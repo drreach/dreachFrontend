5eeeba8f2cb447d950d07071dfeb29201668a92f
technicalranjitofficial <dranjitkumar16@gmail.com>
2024-03-07 09:49:23 +0530

    time issue

--- components/FailedToFetch.tsx
+++ components/FailedToFetch.tsx
@@ -4,7 +4,7 @@ const FailedToFetch = ({message}:{message:string}) => {
   return (
     <div className="min-h-screen  w-full h-full justify-center flex ">
      <div className='  w-full rounded-md justify-center items-center  full flex '>
-    <div className='w-[30rem] p-5 h-auto   rounded-md bg-slate-800'>
+    <div className='w-[30rem] p-5 h-auto   rounded-md border border-gray-700'>
         <h1  className='text-xl'>Error Occured!</h1>
         <p className='pt-6'>{message}</p>
         {/* <p style={paragraph.style} className='pt-6'>{error.message}</p> */}
