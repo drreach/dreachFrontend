49018de259a14ef4c883cffc73655b3c020b3a69
SUDEEPTA SARKAR <67630579+sudsarkar13@users.noreply.github.com>
2024-06-30 00:56:07 +0530

    Prettier formatted

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,38 +1,36 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
-import 'bootstrap/dist/css/bootstrap.css'
+import "bootstrap/dist/css/bootstrap.css";
 import Navbar from "@/components/Navbar";
 import { Providers } from "./provider";
 import Footer from "@/components/Footer";
 import Toaster from "@/components/Toaster";
 
-
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Dreach.in",
-  description: "Dreach.in is a platform for doctors and patients to connect and communicate.",
+	title: "Dreach.in",
+	description:
+		"Dreach.in is a platform for doctors and patients to connect and communicate.",
 };
 
 export default function RootLayout({
-  children,
+	children,
 }: Readonly<{
-  children: React.ReactNode;
+	children: React.ReactNode;
 }>) {
-  return (
-    <html lang="en">
-      <body className="">
-        <Providers>
-        <Navbar/>
-        
-        {children}
+	return (
+		<html lang='en'>
+			<body className=''>
+				<Providers>
+					<Navbar />
 
-
-        </Providers>
-        <Footer/>
-        <Toaster/>
-        </body>
-    </html>
-  );
+					{children}
+				</Providers>
+				<Footer />
+				<Toaster />
+			</body>
+		</html>
+	);
 }
