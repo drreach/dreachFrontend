d3a28801b9b8a90e488f8c434a7ce08a6f33c76b
Abhik <theabhik2020@gmail.com>
2025-04-01 21:34:35 +0530

    main: Added

--- app/admin/dashboard/layout.tsx
+++ app/admin/dashboard/layout.tsx
@@ -17,7 +17,7 @@ export default async function Layout({
   const session = await getServerSession(authOption);
 
   if (!session || session.data.role !== "ADMIN") {
-    return redirect("/");
+    // return redirect("/");
   }
   return (
     <div className="main-wrapper">
